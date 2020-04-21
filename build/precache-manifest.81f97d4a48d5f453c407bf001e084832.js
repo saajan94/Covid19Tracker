77867bbec15a6264d14771581b08928f82d42a08
saajan94 <saajan1994@aol.com>
2020-04-21 17:34:30 -0400

    Change app title

---
+++ build/precache-manifest.81f97d4a48d5f453c407bf001e084832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9306e5d86c8c43f3e815e0656cb49f0a",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "59535d185eafae68943c",
+    "url": "/Covid19Tracker/static/css/main.1b41bccb.chunk.css"
+  },
+  {
+    "revision": "4c1c70e938cd1a34dbb8",
+    "url": "/Covid19Tracker/static/js/2.858c194c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.858c194c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59535d185eafae68943c",
+    "url": "/Covid19Tracker/static/js/main.1f05e3dd.chunk.js"
+  },
+  {
+    "revision": "ae742fae7e2a8235a6e6",
+    "url": "/Covid19Tracker/static/js/runtime-main.253982a6.js"
+  },
+  {
+    "revision": "bc71574bb84f2ea467520ca59384b532",
+    "url": "/Covid19Tracker/static/media/SARS-CoV-2.bc71574b.png"
+  }
+]);
